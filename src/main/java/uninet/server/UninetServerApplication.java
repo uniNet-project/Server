94f727a47ecab44e7e53d178748765bd274b2618
CYY1007 <younguk86@gmail.com>
2023-07-08 01:35:15 +0900

    프로젝트 생성, CI : ci/cd 파이프라인 구축

---
+++ src/main/java/uninet/server/UninetServerApplication.java
@@ -0,0 +1,13 @@
+package uninet.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UninetServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UninetServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uninet/server/UninetServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uninet.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UninetServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
